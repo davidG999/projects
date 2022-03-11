02f978ce86cc3fe1f41524fb1e78deb9c273fdde
DavidGit999 <itprog999@gmail.com>
2022-03-11 20:20:13 +0200

    Create const buttons

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const source = document.querySelectorAll('.source')
+const projectsPreviews = document.querySelectorAll('.project__preview')
 
 source.forEach(e => e.addEventListener('mouseenter', e => {
   e.target.querySelector('[data-gh-icon]').style.filter = 'invert(100%)'
@@ -7,3 +8,16 @@ source.forEach(e => e.addEventListener('mouseenter', e => {
 source.forEach(e => e.addEventListener('mouseleave', e => {
   e.target.querySelector('[data-gh-icon]').style.filter = 'invert(0%)'
 }))
+
+const sourceResponsive = `
+  <div class="project__source" data-responsive>
+    <a href="https://github.com/davidG999/hover-board" class="source" target="_blank">View source code
+      <img data-gh-icon src="github-icons/GitHub-Mark-Light-32px.png" alt="GitHub icon">
+    </a>
+  </div>
+  `
+const tryItResponsive = `
+  <div class="project__try-it" data-responsive>
+    <a href="https://davidg999.github.io/hover-board" class="try-it" target="_blank">Try it out yourself! 🎨</a>
+  </div>
+  `
