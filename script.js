c33569237992683973ea68cfcae3a80a99c01fc0
DavidGit999 <itprog999@gmail.com>
2022-03-11 21:20:42 +0200

    Put unique variables in project__info template

--- script.js
+++ script.js
@@ -4,7 +4,21 @@ const projectsPreviews = document.querySelectorAll('.project__preview')
 let uniqueGHLink = '',
   uniqueTryItLink = '',
   uniqueEmoji = '',
-  uniqueTitle = ''
+  uniqueTitle = '';
+
+const projectInfo = `
+  <div class="project__info">
+    <h2>${uniqueTitle}</h2>
+    <div class="project__source" data-default>
+      <a href="https://github.com/davidG999/${uniqueGHLink}" class="source" target="_blank">View source code
+        <img data-gh-icon src="github-icons/GitHub-Mark-Light-32px.png" alt="GitHub icon">
+      </a>
+    </div>
+    <div class="project__try-it" data-default>
+      <a href="${uniqueTryItLink}" class="try-it" target="_blank">Try it out yourself! ${uniqueEmoji}</a>
+    </div>
+  </div>
+  `;
 
 source.forEach(e => e.addEventListener('mouseenter', e => {
   e.target.querySelector('[data-gh-icon]').style.filter = 'invert(100%)'
@@ -14,18 +28,7 @@ source.forEach(e => e.addEventListener('mouseleave', e => {
   e.target.querySelector('[data-gh-icon]').style.filter = 'invert(0%)'
 }))
 
-const sourceResponsive = `
-  <div class="project__source" data-responsive>
-    <a href="https://github.com/davidG999/hover-board" class="source" target="_blank">View source code
-      <img data-gh-icon src="github-icons/GitHub-Mark-Light-32px.png" alt="GitHub icon">
-    </a>
-  </div>
-  `
-const tryItResponsive = `
-  <div class="project__try-it" data-responsive>
-    <a href="https://davidg999.github.io/hover-board" class="try-it" target="_blank">Try it out yourself! 🎨</a>
-  </div>
-  `
+
 
 if (document.body.clientWidth <= 995) {
   projectsPreviews.forEach(p => p.insertAdjacentHTML('afterend', `${sourceResponsive} ${tryItResponsive}`))
