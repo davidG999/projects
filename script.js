906a19dbf09ceb6e4220504c4e689a60df8e4859
DavidGit999 <itprog999@gmail.com>
2022-03-22 12:49:05 +0200

    Show hover board preview after confirmation

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const source = document.querySelectorAll('.source')
+const hoverPreview = document.querySelector('[data-hover-preview]')
 
 source.forEach(e => e.addEventListener('mouseenter', e => {
   e.target.querySelector('[data-gh-icon]').style.filter = 'invert(100%)'
@@ -7,3 +8,8 @@ source.forEach(e => e.addEventListener('mouseenter', e => {
 source.forEach(e => e.addEventListener('mouseleave', e => {
   e.target.querySelector('[data-gh-icon]').style.filter = 'invert(0%)'
 }))
+
+hoverPreview.querySelector('[data-confirmation]').addEventListener('click', function () {
+  hoverPreview.firstElementChild.remove()
+  hoverPreview.lastElementChild.style.display = 'block'
+})
